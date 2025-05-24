27709ec795138cef4928417ab5cc806a544ef518
danaroshrosh <roshdana12@gmail.com>
2025-05-24 15:38:49 +0300

    models

---
+++ src/model/SupplierInventoryLink.java
@@ -0,0 +1,27 @@
+package model;
+
+public class SupplierInventoryLink {
+    private String supplierId;
+    private String serialNum;
+
+    public SupplierInventoryLink(String supplierId, String serialNum) {
+        this.supplierId = supplierId;
+        this.serialNum = serialNum;
+    }
+
+    public String getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(String supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getSerialNum() {
+        return serialNum;
+    }
+
+    public void setSerialNum(String serialNum) {
+        this.serialNum = serialNum;
+    }
+}
